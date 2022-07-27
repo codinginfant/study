fb634cfc5c5ee2f1f3762762887ade1b806acce1
codinginfant <codinginfant@gmail.com>
2022-07-27 21:11:18 +0900

    Java 스택

---
+++ Section2/Stack/src/Stack/ImplementationTree.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class ImplementationTree {
+    public static void main(String[] args) {
+        Comparator.comparing(i -> i)
+        System.out.println();
+    }
+}
